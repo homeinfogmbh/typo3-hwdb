37441cfebd4c7dee3fb5fdb1ae6ad9a66967a95b
Richard Neumann <r.neumann@homeinfo.de>
2023-01-04 11:11:00 +0100

    Use mdb to select addresses

--- Classes/Domain/Repository/DeploymentRepository.php
+++ Classes/Domain/Repository/DeploymentRepository.php
@@ -32,37 +32,15 @@ class DeploymentRepository
     public function list(): Generator {
         foreach ($this->select()->executeQuery()->fetchAll() as &$record)
         {
-            yield Deployment::fromJoinedRecord($record);
+            $address = AddressRepository.get($record['address']);
+            $lpt_address = (($lpt_address = $record['lpt_address']) === null) ? null : AddressRepository.get($lpt_address);
+            yield Deployment::fromArray($record, $address, $lpt_address);
         }
     }
 
     private function select(): QueryBuilder {
         return ($queryBuilder = $this->connectionPool->getQueryBuilderForTable('deployment'))
-            ->select(
-                'deployment.*',
-                'address.street AS address_street',
-                'address.house_number AS address_house_number',
-                'address.zip_code AS address_zip_code',
-                'address.city AS address_city',
-                'address.district AS address_district',
-                'lpt_address.street AS address_street',
-                'lpt_address.house_number AS lpt_address_house_number',
-                'lpt_address.zip_code AS lpt_address_zip_code',
-                'lpt_address.city AS lpt_address_city',
-                'lpt_address.district AS lpt_address_district',
-            )
-            ->from('deployment')
-            ->join(
-                'deployment',
-                'address',
-                'address',
-                $queryBuilder->expr()->eq('address.id', $queryBuilder->quoteIdentifier('deplyoment.address'))
-            )
-            ->leftJoin(
-                'deployment',
-                'address',
-                'lpt_address',
-                $queryBuilder->expr()->eq('lpt_address.id', $queryBuilder->quoteIdentifier('deployment.lpt_address'))
-            );
+            ->select('*')
+            ->from('deployment');
     }
 }
