55586cfde64c14e902b073e43142169a51fe50de
Richard Neumann <r.neumann@homeinfo.de>
2023-01-25 10:15:10 +0100

    Add action buttons

--- ext_tables.php
+++ ext_tables.php
@@ -8,11 +8,27 @@ defined('TYPO3_MODE') || die();
     '',
     '',
     [
-        \Homeinfo\hwdb\Controller\DebugController::class => 'index',
+        \Homeinfo\hwdb\Controller\DebugController::class => 'listDeployments',
     ],
     [
         'access' => 'user',
         'labels' => 'LLL:EXT:hwdb/Resources/Private/Language/locallang_be.xlf:backend.deployments.label',
+        'inheritNavigationComponentFromMainModule' => false,
+        'standalone' => true,
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'hwdb',
+    'hwdb',
+    '',
+    '',
+    [
+        \Homeinfo\hwdb\Controller\DebugController::class => 'listSystems',
+    ],
+    [
+        'access' => 'user',
+        'labels' => 'LLL:EXT:hwdb/Resources/Private/Language/locallang_be.xlf:backend.systems.label',
         'inheritNavigationComponentFromMainModule' => false,
         'standalone' => true,
     ]
