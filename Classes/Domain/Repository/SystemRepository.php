7b2c78b99045c591b61cab7f25f9a2abfca057d9
Richard Neumann <r.neumann@homeinfo.de>
2023-01-25 10:05:31 +0100

    Implement findByDeploymentIds

---
+++ Classes/Domain/Repository/SystemRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Homeinfo\hwdb\Domain\Repository;
+
+use Generator;
+
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+
+use Homeinfo\hwdb\Domain\Model\System;
+
+class SystemRepository
+{
+    private AddressRepository $addressRepository;
+
+    public function __construct(
+        private readonly ConnectionPool $connectionPool
+    ) {
+        $this->addressRepository = new AddressRepository($connectionPool);
+    }
+
+    public function findById(int $id): array {
+        return ($queryBuilder = $this->select())
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'id',
+                    $queryBuilder->createNamedParameter($id, Connection::PARAM_INT)
+                )
+            )
+            ->executeQuery()
+            ->fetch();
+    }
+
+    public function findByDeploymentId(int $deploymentId): array {
+        return ($queryBuilder = $this->select())
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'deployment',
+                    $queryBuilder->createNamedParameter($deploymentId, Connection::PARAM_INT)
+                )
+            )
+            ->executeQuery()
+            ->fetchAll();
+    }
+
+    public function findByDeploymentIds(array $deploymentIds): array {
+        return ($queryBuilder = $this->select())
+            ->where(
+                $queryBuilder->expr()->in(
+                    'deployment',
+                    $queryBuilder->createNamedParameter($deploymentIds, Connection::PARAM_INT_ARRAY)
+                )
+            )
+            ->executeQuery()
+            ->fetchAll();
+    }
+
+    public function list(): Generator {
+        $addressMap = $this->addressRepository->getMap();
+
+        foreach ($this->select()->executeQuery()->fetchAll() as &$record)
+        {
+            yield Deployment::fromArray(
+                $record,
+                $addressMap[$record['address']],
+                (($lpt_address = $record['lpt_address']) === null) ? null : $addressMap[$lpt_address]
+            );
+        }
+    }
+
+    private function select(): QueryBuilder {
+        return ($queryBuilder = $this->connectionPool->getQueryBuilderForTable('deployment'))
+            ->select('*')
+            ->from('deployment');
+    }
+}
