54c49cdd92ab8dc921d9c57f6b7e380d2c214d45
Richard Neumann <r.neumann@homeinfo.de>
2023-03-17 10:32:54 +0100

    Code style

--- Classes/Domain/Repository/DeploymentRepository.php
+++ Classes/Domain/Repository/DeploymentRepository.php
@@ -51,7 +51,7 @@ class DeploymentRepository
                 )
                 ->executeQuery()
                 ->fetchAll() as $record
-            )
+        )
             yield Deployment::fromArray($record);
     }
 
